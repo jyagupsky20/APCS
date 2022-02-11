b4352247617213e3d9676e476bac1ccc0c3e654a
jyagupsky20 <jyagupsky20@stuy.edu>
2022-02-10 23:59:56 -0500

    Started review

---
+++ cobo/review/Driver.java
@@ -0,0 +1,11 @@
+public class Driver{
+  public static void main(String[] args) {
+    /*System.out.println(Review.sentimentVal("happily"));
+    System.out.println(Review.sentimentVal("terrible"));
+    System.out.println(Review.sentimentVal("cold"));
+    System.out.println(Review.sentimentVal("dreadful"));
+    System.out.println(Review.sentimentVal("refreshing"));
+    System.out.println(Review.sentimentVal("melancholy"));*/
+    System.out.println(Review.totalSentiment("SimpleReview.txt"));
+  }
+}
