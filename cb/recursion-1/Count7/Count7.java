0a913d70e1c7550ca00187283d71c8f362a65828
jyagupsky20 <jyagupsky20@stuy.edu>
2022-02-14 21:37:47 -0500

    All the code assembled

---
+++ cb/recursion-1/Count7/Count7.java
@@ -0,0 +1,15 @@
+public class Count7{
+  public static int count7(int n) {
+    if (n == 0) {
+      return 0;
+    }
+    if (n % 10 != 7){
+      return (0 + count7(n / 10));
+    }
+    else if (n % 10 == 7) {
+      return ( 1 + count7(n / 10) );
+    }
+    return 0;
+
+  }
+}
