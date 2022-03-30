71938f299225f29ffe06bf399864cc94ac2aba41
jyagupsky20 <jyagupsky20@stuy.edu>
2022-03-29 20:19:24 -0400

    HW#84

--- 84_stack/LatKtS.java
+++ 84_stack/LatKtS.java
@@ -15,6 +15,15 @@ public class LatKtS
    **/
   public static String flip( String s )
   {
+    String out = "";
+    Latkes stack = new Latkes(s.length());
+    for (int i = 0; i < s.length(); i++) {
+      stack.push(s.charAt(i)+"");
+    }
+    while (!stack.isEmpty()){
+      out = out + stack.pop();
+    }
+    return out;
   }
 
 
@@ -26,13 +35,42 @@ public class LatKtS
    **/
   public static boolean allMatched( String s )
   {
+    String cur = "";
+    Latkes stack = new Latkes(s.length());
+    for (int i = 0; i < s.length(); i++) {
+      cur = s.charAt(i) + "";
+      if (isOpenBrack(cur)) {
+        stack.push(cur);
+      }
+      else if (isCloseBrack(cur) && (stack.isEmpty() || !flipOpen(stack.pop()).equals(cur))) {
+        return false;
+      }
+    }
+    return stack.isEmpty();
   }
-
-
+  //Helper functions for allMatched
+  private static boolean isOpenBrack(String s){
+    return s.equals("{") || s.equals("(") || s.equals("[");
+  }
+  private static boolean isCloseBrack(String s){
+    return s.equals("}") || s.equals(")") || s.equals("]");
+  }
+  private static String flipOpen(String s){
+    if (s.equals("(")){
+      return ")";
+    }
+    if (s.equals("{")){
+      return "}";
+    }
+    if (s.equals("[")){
+      return "]";
+    }
+    //Should never happen:
+    return "";
+  }
   //main method to test
   public static void main( String[] args )
   {
-    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
     System.out.println(flip("stressed"));
     System.out.println(allMatched( "({}[()])" )); //true
     System.out.println(allMatched( "([)]" ) ); //false
@@ -40,7 +78,6 @@ public class LatKtS
     System.out.println(allMatched( "](){([])}" ) ); //false
     System.out.println(allMatched( "(){([])}(" ) ); //false
     System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
-      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
   }
 
 }//end class
