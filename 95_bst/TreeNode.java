23bba5d9f44019c5b79d3edebe31dce62f97484d
jyagupsky20 <jyagupsky20@stuy.edu>
2022-05-09 21:35:14 -0400

    HW#95

---
+++ 95_bst/TreeNode.java
@@ -0,0 +1,102 @@
+/*
+Team Cautious Crustaceans :: Orion Roven, Joshua Yagupsky, Johnathan Song
+APCS pd7
+HW95 -- Algorithm as Data Structure
+2022-05-10t
+time elapsed: .5 hrs
+*/
+
+/**
+ * class TreeNode
+ * SKELETON
+ * Implements a node for use in a binary tree.
+ * Only holds int cargo.
+ */
+
+public class TreeNode
+{
+
+  //instance variables / attributes of a TreeNode:
+  int _cargo;        //this node's data
+  TreeNode _lt, _rt; //pointers to left, right subtrees
+
+
+  /**
+   * default constructor
+   * Construct a tree node with specified value,
+   * with null left and right subtrees.
+   */
+  TreeNode( int initValue )
+  {
+    this._cargo = initValue;
+    _lt = null;
+    _rt = null;
+  }
+
+
+  /**
+   * overloaded constructor
+   * Construct tree node w specified value, left, and right subtrees.
+   */
+  TreeNode( int initValue, TreeNode initLeft, TreeNode initRight )
+  {
+    this._cargo = initValue;
+    this._lt = initLeft;
+    this._rt = initRight;
+  }
+
+
+  /**
+   * Returns the left subtree of this node.
+   */
+  TreeNode getLeft()
+  {
+    return this._lt;
+  }
+
+
+  /**
+   * Returns the right subtree of this node.
+   */
+  TreeNode getRight()
+  {
+    return this._rt;
+  }
+
+
+  /**
+   * Returns the value stored in this tree node.
+   */
+  int getValue()
+  {
+    return this._cargo;
+  }
+
+
+  /**
+   * Sets the value of the left subtree of this node.
+   */
+  void setLeft( TreeNode theNewLeft )
+  {
+    this._lt = theNewLeft;
+  }
+
+
+  /**
+   * Sets the value of the right subtree of this node.
+   */
+  void setRight( TreeNode theNewRight )
+  {
+    this._rt = theNewRight;
+  }
+
+
+  /**
+   * Sets the value of this tree node.
+   */
+  void setValue( int theNewValue )
+  {
+    this._cargo = theNewValue;
+  }
+
+}//end class
