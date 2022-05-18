c533f969946df33d0a9ebcd681a6c3a084f3f42a
jyagupsky20 <jyagupsky20@stuy.edu>
2022-05-17 22:06:19 -0400

    Sifting up and down

--- 102_heap/ALHeap.java
+++ 102_heap/ALHeap.java
@@ -1,10 +1,25 @@
+/*
+Team Cautious Crustaceans :: Orion Roven, Joshua Yagupsky, Jonathan Song
+APCS pd7
+HW102: Heap On Heapin' On
+2022-05-18w
+time spent: .6 hrs
+
+DISCO:
+* If we add or remove using the methods we discussed in class, the heap not only maintains the heap property, but also is a complete tree.
+QCC:
+* Is there a more efficient way to create a heap out of a collection of elements than adding them one at a time?
+*/
+
 /**
  * class ALHeap
  * SKELETON
  * Implements a min heap using an ArrayList as underlying container
  */
 
-//import java.util.ArrayList;
+
+
+import java.util.ArrayList;
 
 public class ALHeap
 {
@@ -32,7 +47,7 @@ public class ALHeap
   {
 	  String s = "";
 	  for ( int item : _heap) {
-	  	s+= item + " ";
+	  	s += item + " ";
 	  }
 	  return s;
   }//O(n)
@@ -44,7 +59,7 @@ public class ALHeap
    */
   public boolean isEmpty()
   {
-	  return _heap.size()==0
+	  return _heap.size() == 0;
   }//O(1)
 
 
@@ -55,7 +70,11 @@ public class ALHeap
    */
   public Integer peekMin()
   {
-	  if(!isEmpty()){ return _heap.get(0);} else {return null;}
+	  if ( !isEmpty() ) {
+      return _heap.get(0);
+    } else {
+      return null;
+    }
   }//O(1)
 
 
@@ -64,14 +83,20 @@ public class ALHeap
    * Inserts an element in the heap
    * Postcondition: Tree exhibits heap property.
    * ALGO:
-   * <your clear && concise procedure here>
+   * Add node at leftmost unused spot and continue to swap the added node with its parent if if is less than its parent
    */
   public void add( Integer addVal )
   {
 	  _heap.add(addVal);
-	  currentIndex = _heap.size()-1;
-	  while(_heap.get(
-  }//O(?)
+	  int currentIndex = _heap.size()-1;
+    int parentIndex = (currentIndex -1) / 2;
+    Integer temp;
+	  while(currentIndex >= 0 && parentIndex >= 0 && _heap.get(currentIndex) < _heap.get(parentIndex)) {
+      swap(currentIndex, parentIndex);
+      currentIndex = parentIndex;
+      parentIndex = (currentIndex -1) / 2;
+    }
+  }//O(h), where h is the tree height
 
 
   /**
@@ -79,11 +104,24 @@ public class ALHeap
    * Removes and returns least element in heap.
    * Postcondition: Tree maintains heap property.
    * ALGO:
-   * <your clear && concise procedure here>
+   * Move the root out, and move in the last element to replace the root. While the element still has children, and while the root is bigger than its smallest child, swap it with its smallest child.
    */
   public Integer removeMin()
   {
-  }//O(?)
+    if (_heap.size() == 0){
+      return null;
+    }
+    Integer out = _heap.remove(0); //Get the root out
+    if (_heap.size() != 0) {
+      _heap.add(0, _heap.remove(_heap.size()-1)); //Move in element from the bottom
+      int currentIndex = 0;
+      while(minChildPos(currentIndex) != -1 && _heap.get(currentIndex) > _heap.get(minChildPos(currentIndex))){
+        swap(currentIndex, minChildPos(currentIndex));
+        currentIndex = minChildPos(currentIndex);
+      }
+    }
+    return out;
+  }//O(h), where h is the tree height
 
 
   /**
@@ -94,7 +132,21 @@ public class ALHeap
    */
   private int minChildPos( int pos )
   {
-  }//O(?)
+    if(2*pos+2 < _heap.size()) {
+      if (_heap.get(2*pos+2) < _heap.get(2*pos+1)){
+        return 2*pos+2;
+      }
+      else {
+        return 2*pos+1;
+      }
+    }
+    else if(2*pos+1 < _heap.size()){
+      return 2*pos+1;
+    }
+    else {
+      return -1;
+    }
+  }//O(1)
 
 
   //~~~~~~~~~~~~~ aux helper fxns ~~~~~~~~~~~~~~
@@ -109,7 +161,7 @@ public class ALHeap
   //swap for an ArrayList
   private void swap( int pos1, int pos2 )
   {
-    _heap.set( pos1, _heap.set( pos2, _heap.get(pos1) ) );	
+    _heap.set( pos1, _heap.set( pos2, _heap.get(pos1) ) );
   }
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -118,7 +170,6 @@ public class ALHeap
   //main method for testing
   public static void main( String[] args )
   {
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       ALHeap pile = new ALHeap();
 
       pile.add(2);
@@ -164,7 +215,6 @@ public class ALHeap
       System.out.println(pile);
       System.out.println("removing " + pile.removeMin() + "...");
       System.out.println(pile);
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }//end main()
 
 }//end class ALHeap
